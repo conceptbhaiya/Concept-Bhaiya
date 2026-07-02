dc435a55b62aeda82c54b02d9e13e7c9c413de67
conceptbhaiya <2003rohitkhandelwal@gmail.com>
2026-07-02 08:34:16 +0530

    Update concept-challenge.js

--- concept-challenge.js
+++ concept-challenge.js
@@ -1,1 +1,63 @@
+const questions = [
 
+{
+question: "Who first observed the nucleus in plant cells?",
+options: [
+"Friedrich Miescher",
+"Robert Brown",
+"Altmann",
+"Watson"
+],
+answer: 1,
+explanation: "Robert Brown first observed the nucleus in plant cells in 1831."
+},
+
+{
+question: "Which scientist isolated a phosphorus-rich substance from pus cells that was later identified as DNA?",
+options: [
+"Robert Brown",
+"Altmann",
+"Friedrich Miescher",
+"Chargaff"
+],
+answer: 2,
+explanation: "Friedrich Miescher isolated nuclein from pus cells, which was later identified as DNA."
+},
+
+{
+question: "Why did Friedrich Miescher name the newly isolated substance 'Nuclein'?",
+options: [
+"It was rich in nitrogen.",
+"It was isolated from the nucleus.",
+"It was acidic in nature.",
+"It was found in nucleolus."
+],
+answer: 1,
+explanation: "He named it nuclein because it was isolated from the nucleus."
+},
+
+{
+question: "Altmann renamed 'Nuclein' as 'Nucleic Acid' because the substance:",
+options: [
+"Was found only in animal cells.",
+"Showed acidic properties.",
+"Was composed entirely of proteins.",
+"Was present only in the nucleus."
+],
+answer: 1,
+explanation: "Altmann renamed nuclein as nucleic acid because of its acidic nature."
+},
+
+{
+question: "Which sequence correctly represents the historical discoveries?",
+options: [
+"Altmann → Brown → Miescher",
+"Robert Brown → Miescher → Altmann",
+"Miescher → Brown → Altmann",
+"Miescher → Altmann → Brown"
+],
+answer: 1,
+explanation: "Robert Brown discovered the nucleus first, then Miescher discovered nuclein, and finally Altmann renamed it nucleic acid."
+}
+
+];
